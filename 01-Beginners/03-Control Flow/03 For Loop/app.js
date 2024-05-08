30c9284bdc2c7abb8312f3522123e6a7b8332ff1
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-05-08 18:44:47 +0530

    For loop

---
+++ 01-Beginners/03-Control Flow/03 For Loop/app.js
@@ -0,0 +1,12 @@
+// Print your name & numbers from 0 through 1000
+
+for (let i = 0; i <= 1000; i++) {
+    console.log("Lokesh WebDev", i);
+  }
+
+  // for (initialExpression; condition; incrementExpression) {}
+
+for (let i = 0; i <= 5; i++) {
+  console.log("Hello World", i);
+}
+
