346ea2a09ae12a73a5e6ce5e046e03d3bed1dda9
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-05-08 18:48:11 +0530

    While loop

---
+++ 01-Beginners/03-Control Flow/04 While Loop/app.js
@@ -0,0 +1,23 @@
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let i = 10;
+
+while (i <= 100) {
+  console.log("Lokesh WebDev");
+  i++;
+}
+
+// while (condition) {
+//   // code block to be executed
+// }
+
+// --------------------------
+let j = 1;
+
+while (j<= 5) {
+  console.log("Hello World", j);
+  j++;
+}
+
+// ---------------------------
