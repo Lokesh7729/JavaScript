4aa4f68f60047a0290301fc3f39d93d0d71b7249
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-07-17 11:22:28 +0530

    color changing project

---
+++ chai_aur_code/03_Events/project/script.js
@@ -0,0 +1,41 @@
+// generate a random color
+const body= document.querySelector('body');
+
+const randomColor= function(){
+    const hex= "0123456789ABCDEF"
+    let color= '#'
+    for(let i=0; i<6;i++){
+        color+= hex[Math.floor(Math.random()* 16)]
+    }
+    return color;
+}
+let intervalId;
+const startChangingColor= 
+function(){
+
+    if(!intervalId){
+        intervalId= setInterval(() => {
+            body.style.backgroundColor= randomColor();
+            body.style.color= randomColor();
+            
+        }, 1000);
+    }
+
+   
+
+}
+
+const stopChangingColor= function(){
+    clearInterval(intervalId);
+    intervalId=null;
+
+}
+
+
+document.querySelector("#start").addEventListener('click', startChangingColor)
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
+
+
+
+ 
