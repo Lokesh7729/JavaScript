2a3c7028c47be84373bf26b89c72fafca2882c8c
Lokesh <lokeshchauhan7729@gmail.com>
2024-10-13 23:18:07 +0530

    first class functions

---
+++ Namaste Js/firstClassFunction.js
@@ -0,0 +1,38 @@
+// function Statement aka Function Declaration
+
+function a() {
+  console.log("function statement !");
+}
+a();
+
+// Function Expression
+
+var b = function () {
+  console.log("function Expression !");
+};
+b();
+
+// Anonymous Function
+// function(){
+
+// }
+
+// Named Function Expression
+
+// Diffrence between parameters & arguments ?
+
+function paraFunc(param1, param2) {
+  // gets those values
+  console.log(param1, param2);
+}
+paraFunc(10, 20); // values that we pass inside a function
+
+// First Class Functions
+
+// the ability to use functions as values .
+
+// the ability of fucntions to be used as values and can be passed as an aguments and can be returned from an function .
+
+// first class citizens
+
+ 
