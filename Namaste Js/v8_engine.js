b315d09cd191ee6ecdfb6c660927f23f30e7931c
Lokesh <lokeshchauhan7729@gmail.com>
2024-10-14 16:06:09 +0530

    v8 engine

---
+++ Namaste Js/v8_engine.js
@@ -0,0 +1,22 @@
+// ! JIT -> JUST IN TIME COMPILATION
+// Interpreter -> line by line || fast
+// Compiler  -> code is compiled before execution || efficency ||optimize the code
+
+// aot -> ahead of time compilation
+
+// memory heap and call stack
+
+// ---------------
+
+// JS CODE ->
+// PARSING -> AST (abstract syntax tree)
+// COMPILATION -> INTERPRETTER (BYTE CODE ) -> COMPILER -> OPTIMIZE CODE -> BYTE CODE
+// EXECUTION ->
+
+// garbage collector
+// ! mark and sweep algo
+// inlining
+// copy elision
+// inline caching
+
+// -----------------
