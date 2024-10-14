49f837cb1af510cabbc9d433d5ecbcadb513bfdb
Lokesh <lokeshchauhan7729@gmail.com>
2024-10-14 16:05:32 +0530

    event loop

---
+++ Namaste Js/asynchronous_Js_eventLoops.js
@@ -0,0 +1,5 @@
+// browser -> js engine -> callstack
+// browser have local storage , url ,
+
+// all the callback functions that comes from promises as well as mutation observer  [ microtask queue ]
+
